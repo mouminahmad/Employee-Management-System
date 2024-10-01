f3ee2414c127cdb8d3cd103ed64181872c6038b0
MOUMIN <116239910+Moumin-pk@users.noreply.github.com>
2024-10-01 19:41:21 +0500

    create master services

--- src/app/Features/employee/employee.component.ts
+++ src/app/Features/employee/employee.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component , OnInit,inject, signal} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IApiResponse, IParentDept } from '../../model/interface/master';
+import { MasterService } from '../../service/master.service'; 
+
 @Component({
   selector: 'app-employee',
   standalone: true,
@@ -7,8 +10,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
-export class EmployeeComponent {
+export class EmployeeComponent implements OnInit {
 
   // Property to toggle form visibility
   showForm = false;
+  
+  masterserv = inject(MasterService)
+  parentDeptList  = signal<IParentDept[]>([])
+
+  ngOnInit(): void {
+    this.getParentDept();
+  }
+
+  getParentDept(){
+    this.masterserv.getAllDept().subscribe((res:IApiResponse) => {
+     this.parentDeptList.set(res.data);
+    })
+  }
+
   }
